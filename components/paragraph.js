034cfea06408d9e7a941bba48a56678319c1a87b
Zhenya Pastukhov <amamahahatala@hotmail.com>
2022-12-22 09:16:53 +0200

    ok

--- components/paragraph.js
+++ components/paragraph.js
@@ -6,4 +6,11 @@ text-indent:1em;
 font-family: DynaPuff;
 `
 
+export const ParagraphError = styled.p`
+color: red;
+text-align:justify;
+text-indent:1em;
+font-family: DynaPuff;
+`
+
 export default Paragraph
--- pages/contact.js
+++ pages/contact.js
@@ -1,44 +1,63 @@
-import { Button, Container, FormControl, FormLabel, Heading } from "@chakra-ui/react";
+import { Button, Container, FormControl, FormErrorMessage, FormLabel, GridItem, Heading, Input } from "@chakra-ui/react";
 import { Field, Form, Formik } from "formik";
+ import * as Yup from 'yup';
 import Layout from "../components/layouts/article";
+import { ParagraphError } from "../components/paragraph";
 import Section from "../components/section";
 
+const rePhoneNumber = /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}?$/;
 
-
+const SignupSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  number: Yup.string()
+    .matches(rePhoneNumber, 'Phone number is not valid, example +380123456789')
+    .required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+});
 
 function FormToContact() {
-
+  
   return (
   <Section>
     <Formik
       initialValues={{
-        firstName: '',
+        name: '',
         number: '',
         email: '',
-      }}
+        }}
+        validationSchema={SignupSchema}
       onSubmit={async (values) => {
-        await new Promise((r) => setTimeout(r, 500));
+        await new Promise((res) => setTimeout(res, 500));
         alert(JSON.stringify(values, null, 2));
       }}
-    >
-        <Form>
-          <FormControl>
-        <FormLabel htmlFor="firstName">Name</FormLabel>
-        <Field class='chakra-input css-1f1trk8' id="firstName" name="firstName" placeholder="Yevhen" />
-
-        <FormLabel htmlFor="number">Number</FormLabel>
-        <Field class='chakra-input css-1f1trk8' id="number" name="number" placeholder="+3801234567" />
-
-        <FormLabel htmlFor="email">Email</FormLabel>
-        <Field
-class='chakra-input css-1f1trk8'           id="email"
-          name="email"
-          placeholder="example@mail.com"
-          type="email"
-        />
-        <Button mt={3} align='center' type="submit">Submit</Button>
-      </FormControl>
-        </Form>
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <FormControl>
+              <FormLabel htmlFor="name">Name</FormLabel>
+              <Field as={Input} id="name" name="name" placeholder="Yevhen" />
+              {errors.name && touched.name ? (
+             <ParagraphError>{errors.name}</ParagraphError>
+           ) : null}
+              <FormLabel htmlFor="number">Number</FormLabel>
+              <Field as={Input} id="number" name="number" placeholder="+3801234567" />
+                {errors.number && touched.number ? (
+             <ParagraphError>{errors.number}</ParagraphError>
+           ) : null}
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Field as={Input}
+                id="email"
+                name="email"
+                placeholder="example@mail.com"
+                type="email"
+              />
+              {errors.email && touched.email ? <ParagraphError>{errors.email}</ParagraphError> : null}
+              <Button mt={3} align='center' type="submit">Submit</Button>
+            </FormControl>
+          </Form>)}
     </Formik>
   </Section>
 
